4f2dc95d0dea21d7f1ee60818509b1d8e86ee48f
jwgerlach00 <jwgerlach00@gmail.com>
2021-11-16 19:30:41 -0500

    Began adding outlier smoother, need to finish. Concatenated dataset.

--- rnn_model.py
+++ rnn_model.py
@@ -12,10 +12,10 @@ import signal_utils
 
 def training_loop():
     # Model params
-    batch_size = 16
+    batch_size = 64
     hidden_size = 64
     num_features = X.shape[0]
-    rnn_layers = 2
+    rnn_layers = 1
 
     # Dataset params
     window = 128  # Analogous with 'sequence'
@@ -23,7 +23,7 @@ def training_loop():
     
     # Training params
     epochs = 50
-    learning_rate = 0.001
+    learning_rate = 0.1
     weight_decay = 1e-8
 
     # Instantiate model
@@ -68,7 +68,7 @@ def training_loop():
 
             # Calculate train loss
             train_loss = criterion(train_predictions, batch_train_y)
-            batch_train_loss_history.append(train_loss.item())
+            batch_train_loss_history.append(float(train_loss.item()))
 
             # Back prop and optimizer step every batch
             train_loss.backward()
@@ -97,7 +97,7 @@ def training_loop():
 
             # Calculate validation loss
             val_loss = criterion(val_predictions, batch_val_y)
-            batch_val_loss_history.append(val_loss.item())
+            batch_val_loss_history.append(float(val_loss.item()))
         val_loss_history.append(sum(batch_val_loss_history) / len(batch_val_loss_history))
 
     # torch.save(model, 'trained_model.pt')
@@ -120,14 +120,18 @@ if __name__ == '__main__':
     print('DEVICE:', device)
 
     # Read in data
-    path = 'train_data/Keller_Emily_Walking4.xlsx'
-    data = pd.read_excel(path, header=None)
+    path = 'merged_data.xlsx'
+    data = pd.read_excel(path)
+    # data = data.iloc[:int(len(data)/20)]
+    
+    # path = 'train_data/Keller_Emily_Walking4.xlsx'
+    # data = pd.read_excel(path, header=None)
 
-    # Format dataset
-    imu = IMUDataset(data)
-    header = imu.grab_imu_header()
-    imu.header_from_dict(header)
-    data = imu.df.copy()
+    # # Format dataset
+    # imu = IMUDataset(data)
+    # header = imu.grab_imu_header()
+    # imu.header_from_dict(header)
+    # data = imu.df.copy()
 
     # Get features and labels and convert to arrays
     feats_to_use = ['Grav1_0','Grav1_1','Grav1_2','Gyro1_0','Gyro1_1','Gyro1_2','Acc1_0','Acc1_1', 'Acc1_2']
@@ -152,9 +156,9 @@ if __name__ == '__main__':
                                                                  [scaled_X_train, scaled_X_val, scaled_X_test]))
 
     # plot data --> low pass filter
-    # print(X_train[:, 1])
-    # plt.plot(range(len(scaled_X_train)), filtered_X_train)
-    # plt.show()
+    print(X_train[:, 1])
+    plt.plot(range(len(scaled_X_train)), filtered_X_train)
+    plt.show()
 
     # first order, high pass 0.5 Hz cut-off
     # scipy.signal.butter
